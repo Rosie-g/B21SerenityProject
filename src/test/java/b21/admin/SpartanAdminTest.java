358fb1e230b9a7fe7960f75d1b0f0e9335e3d3ce
Rosie-g <ruz0408@gmail.com>
2021-04-15 16:41:01 -0700

    updated SpartanAdminTest

--- src/test/java/b21/admin/SpartanAdminTest.java
+++ src/test/java/b21/admin/SpartanAdminTest.java
@@ -2,8 +2,12 @@ package b21.admin;
 
 import static io.restassured.RestAssured.*;
 
+import io.cucumber.java.af.En;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.apache.commons.text.similarity.JaccardSimilarity;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
@@ -33,10 +37,26 @@ public class SpartanAdminTest {
                 when()
                 .get("/spartans");
 
+
+
         Ensure.that("Successful 200 Response ", p -> p.statusCode(200));
-        Ensure.that("Content Type is JSON ", name -> name.contentType(ContentType.JSON));
+        Ensure.that("Response format as expected ", name -> name.contentType(ContentType.JSON));
+        // check json array size
+        Ensure.that("Response has correct size ", vRes->vRes.body("",hasSize(102)));
+
 
     }
+
+    @DisplayName("Test public user GET /spartans endpoint")
+    @Test
+    public void testPublicUserSpartanData(){
+
+        SerenityRest.when().get("/spartans");
+        Ensure.that("Public user should not be able to get all spartans",p-> p.statusCode(401));
+
+    }
+
+
 
     @AfterAll
     public static void cleanup(){
