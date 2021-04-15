570d480dd1b1f0a9a80bde115ab7a18cbbddc5e3
Rosie-g <ruz0408@gmail.com>
2021-04-15 09:09:08 -0700

    Added GitHub Serenity Test

---
+++ src/test/java/b21/github/GitHubTest.java
@@ -0,0 +1,42 @@
+package b21.github;
+
+import io.restassured.RestAssured;
+import net.serenitybdd.junit5.SerenityTest;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+//import static io.restassured.RestAssured.*;
+import static net.serenitybdd.rest.SerenityRest.* ;
+
+@SerenityTest
+public class GitHubTest {
+
+    // SEND GET https://api.github.com/users/CybertekSchool request
+
+    @BeforeAll
+    public static void init(){
+        RestAssured.baseURI = "https://api.github.com";
+    }
+
+    @Test
+    public void testGitHubUser(){
+
+        given().pathParam("user_id" , "CybertekSchool")
+                .log().all().
+                when()
+                .get("/users/{user_id}").
+                then()
+                .log().all()
+                .statusCode(200) ;
+
+    }
+
+    @AfterAll
+    public static void cleanup(){
+        RestAssured.reset();
+    }
+
+
+
+}
